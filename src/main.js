f588fb7e3e5a15582219a384f15e624b7f173b19
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-19 14:39:59 -0500

    Commit despues de instalar bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Cargar BootstrapVue
+Vue.use(BootstrapVue)
+// Cargar BootstrapVue icons
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
