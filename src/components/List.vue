285f2b0f0cfe5afcdec71cdcd2761b70ab698a71
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-19 13:51:57 -0500

    Commit despues de aplicar metodos y eventos

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,15 +3,24 @@
         <div class="box" v-for="e in elements" v-bind:key="e">
             Elemento {{ e }}
         </div>
+        <button v-on:click="findAll">Click</button>
+        {{ msj }}
     </div>
 </template>
 <script>
 export default {
     data(){
         return{
+            msj: "... :(",
             elements: ["Uno", "Dos", "Tres"]
         };
-    }
+    },
+    methods: {
+        findAll: function(){
+            console.log("Hola mundo")
+            this.msj = "Click :)"
+        }
+    },
 }
 </script>
 <style >
