4f798c6cb96ad69ce6a11b6cffa7791320cfb696
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-19 15:23:53 -0500

    Commit despues de crear el coponente del menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header/>
     <!--<Detail/>-->
     <List/>
   </div>
@@ -8,24 +9,16 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import List from './components/List'
+import Header from './components/Header'
 //import Detail from './components/Detail'
 export default {
   name: 'App',
   components: {
     //HelloWorld
     List,
+    Header
     //Detail
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
